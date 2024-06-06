2983d0751930a708627c29f8906e018085bf7fe7
shubhusion <shubham27.sharma03@gmail.com>
2024-06-06 23:59:19 +0530

    Client Setup (Frontend)

--- server/src/models/CommunicationLog.js
+++ server/src/models/CommunicationLog.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const communicationLogSchema = new mongoose.Schema({
-  audience: { type: mongoose.Schema.Types.Mixed, required: true },
+  audience: { type: mongoose.Schema.Types.Mixed},
   message: { type: String, required: true },
   sentAt: { type: Date, default: Date.now },
   status: { type: String, enum: ['SENT', 'FAILED'], default: 'SENT' },

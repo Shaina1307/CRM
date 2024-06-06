fa3600486c73d4ecf9e5369564e5087a668f446c
shubhusion <shubham27.sharma03@gmail.com>
2024-06-06 20:08:26 +0530

    Database Configuration and Node modules added to .gitignore

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error('MONGODB_URI environment variable is not set');
+    }
+
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -12,5 +21,36 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from DB');
+});
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app termination', () => {
+    process.exit(0);
+  });
+});
 
 module.exports = connectDB;

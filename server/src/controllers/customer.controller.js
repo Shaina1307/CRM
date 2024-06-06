555d85bf7a6b89995f31b7df5581ea2e603bb5aa
shubhusion <shubham27.sharma03@gmail.com>
2024-06-06 19:25:13 +0530

    Logic for Customer API

---
+++ server/src/controllers/customer.controller.js
@@ -0,0 +1,11 @@
+const Customer = require('../models/Customer');
+
+exports.createCustomer = async (req, res) => {
+  try {
+    const customer = new Customer(req.body);
+    await customer.save();
+    res.status(201).json(customer);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
